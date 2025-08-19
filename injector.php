44da496122be493d0677becfc1eede05e02fae76
SanC <stefanus.gunawan.guan@gmail.com>
2025-08-19 16:20:38 +0700

    Add injector.php
    Added PHP file to insert data into mySQL DB

---
+++ injector.php
@@ -0,0 +1,31 @@
+<?php
+
+if(isset($_GET["uid"])) {
+   $temperature = $_GET["uid"]; // get temperature value from HTTP GET
+
+   $servername = "localhost";
+   $username = "Arduino";
+   $password = "ArduinoGetStarted.com";
+   $dbname = "db_arduino";
+
+   // Create connection
+   $conn = new mysqli($servername, $username, $password, $dbname);
+   // Check connection
+   if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+   }
+
+   $sql = "INSERT INTO tbl_temp (uid_value, name, points) VALUES ($uid, name, points)";
+
+   if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+   } else {
+      echo "Error: " . $sql . " => " . $conn->error;
+   }
+
+   $conn->close();
+} else {
+   echo "Points is not set";
+}
+
+?>
